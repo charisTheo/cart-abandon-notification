35a959e1fd9b0b82b3e7ffa2e6738f27c42bdadc
charisTheo <dev@charistheo.io>
2019-11-07 14:04:43 +0000

    Removing build folder before running webpack build. Precaching files needed for navigating to the website on second visit.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
@@ -7,7 +6,8 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-// workbox.precaching.precacheAndRoute(['/'], 'GET');
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
 // workbox.routing.registerRoute(
 //   /\.(?:html|js|css|webp|png|jpg|svg|ico)$/,
 //   new workbox.strategies.StaleWhileRevalidate()
